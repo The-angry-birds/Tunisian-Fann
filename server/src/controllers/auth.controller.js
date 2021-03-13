d16708eae2f2ddecfbec2c4c80203789463f7903
ines <piazzese.ines@gmail.com>
2021-03-13 18:26:33 +0100

    extra code for hashsync to save in data

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,16 +1,22 @@
 const { Admin } = require("../../db/models/admin.model");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const saltRounds = 10;
 const config = require("../../db/config");
 module.exports = {
   login: async (req, res) => {
+    //these lines of codes are just for hashing the password in the database
+    // const salt = bcrypt.genSaltSync(saltRounds);
+    // const hash = bcrypt.hashSync(req.body.password, salt);
+    // const data = Admin.build({ email: req.body.email, password: hash });
+    // var that = await data.save();
+    // res.send(that);
     try {
       var admindata = req.body;
       const admin = await Admin.findOne({ where: { email: admindata.email } });
-
       if (admin) {
-        // bcrypt.compare(admindata.password, admin.password).then((result) => {
-        if (admindata.password === admin.password) {
+        var result = bcrypt.compareSync(admindata.password, admin.password);
+        if (result) {
           var token = jwt.sign({ email: admindata.email }, config.secret, {
             expiresIn: 86400, // expires in 24 hours
           });
@@ -27,27 +33,30 @@ module.exports = {
       console.error(err);
     }
   },
-  signup: async (req, res) => {
-    var hashedPassword = bcrypt.hashSync(req.body.password, 8);
-    try {
-      const user = await Admin.create({
-        email: req.body.email,
-        password: hashedPassword,
-      });
-      if (user) {
-        var token = jwt.sign({ email: req.body.email }, config.secret, {
-          expiresIn: 86400, // expires in 24 hours
-        });
-        res.send({ auth: true, token: token });
-      } else {
-        res.send({
-          message: "There was a problem registering the user",
-          auth: false,
-          token: null,
-        });
-      }
-    } catch (err) {
-      console.error(err);
-    }
+  //   signup: async (req, res) => {
+  //     var hashedPassword = bcrypt.hashSync(req.body.password, 8);
+  //     try {
+  //       const user = await Admin.create({
+  //         email: req.body.email,
+  //         password: hashedPassword,
+  //       });
+  //       if (user) {
+  //         var token = jwt.sign({ email: req.body.email }, config.secret, {
+  //           expiresIn: 86400, // expires in 24 hours
+  //         });
+  //         res.send({ auth: true, token: token });
+  //       } else {
+  //         res.send({
+  //           message: "There was a problem registering the user",
+  //           auth: false,
+  //           token: null,
+  //         });
+  //       }
+  //     } catch (err) {
+  //       console.error(err);
+  //     }
+  // },
+  verify: (req, res) => {
+    res.send("hello");
   },
 };
--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -3,6 +3,6 @@ const verifyToken = require("../../db/middleware/verifyToken");
 const controller = require("../controllers/auth.controller.js");
 
 Router.post("/login", controller.login);
-Router.post("/register", controller.signup);
-
+// Router.post("/register", controller.signup);
+Router.get("/verify", verifyToken, controller.verify);
 module.exports = Router;
