a86eb8c09b2908d54cd405530989df9da80a6b9a
Nabil-Elbir <nabil.elbir@gmail.com>
2021-03-13 18:07:52 +0100

    added carousel

---
+++ Users-Artist/src/components/NavigationBar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+  <div class="navigationBar fixed-top">
+    <img
+      class="logo"
+      src="https://logodownload.org/wp-content/uploads/2014/10/youtube-logo-2-3.png"
+      alt="#"
+    />
+  </div>
+  </div>
+  
+  
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+.navigationBar {
+  background-color: #f0eff4;
+  height: 50px;
+  width: 100%;
+  box-shadow: 0px 0px 10px #404040;
+}
+
+.logo {
+  max-width: 100%;
+  max-height: 100%;
+  position: absolute;
+  /* CHECK with the real logo */
+  left: 45%;     
+  padding: 5px 
+}
+
+
+</style>
