50f90274a6411f0b493d681512a9b87674421a87
Nabil-Elbir <nabil.elbir@gmail.com>
2021-03-14 20:10:50 +0100

    fixed categories card + minor carousel fixes

--- Users/src/components/NavigationBar.vue
+++ Users/src/components/NavigationBar.vue
@@ -3,7 +3,7 @@
   <div class="navigationBar fixed-top">
     <img
       class="logo"
-      src="https://logodownload.org/wp-content/uploads/2014/10/youtube-logo-2-3.png"
+      src="https://www.darest.com/wp-content/uploads/2019/04/Logo-Roomz-Blanc.png"
       alt="#"
     />
   </div>
@@ -35,7 +35,7 @@ export default {};
   max-height: 100%;
   position: absolute;
   /* CHECK with the real logo */
-  left: 45%;     
+  left: 40%;     
   padding: 5px 
 }
 
