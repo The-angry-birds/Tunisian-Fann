846867d8f3c7ac0688289fa9b3f5a1a63af125cd
MalekWahmi <wahmimalek@gmail.com>
2021-03-14 22:13:28 +0100

    fixed-category-data

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -5,12 +5,13 @@ const saltRounds = 10;
 const config = require("../../db/config");
 module.exports = {
   login: async (req, res) => {
-    //these lines of codes are just for hashing the password in the database
-    // const salt = bcrypt.genSaltSync(saltRounds);
-    // const hash = bcrypt.hashSync(req.body.password, salt);
-    // const data = Admin.build({ email: req.body.email, password: hash });
-    // var that = await data.save();
-    // res.send(that);
+    // these lines of codes are just for hashing the password in the database
+  //   const salt = bcrypt.genSaltSync(saltRounds);
+  //   const hash = bcrypt.hashSync(req.body.password, salt);
+  //   const data = Admin.build({ email: req.body.email, password: hash });
+  //   var that = await data.save();
+  //   res.send(that);
+  // }
     try {
       var admindata = req.body;
       const admin = await Admin.findOne({ where: { email: admindata.email } });
@@ -32,7 +33,7 @@ module.exports = {
     } catch (err) {
       console.error(err);
     }
-  },
+  }
   //   signup: async (req, res) => {
   //     var hashedPassword = bcrypt.hashSync(req.body.password, 8);
   //     try {
