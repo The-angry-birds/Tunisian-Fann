abc8eafd59e37eb9b677a6420ba6cc17b95c0872
ines <piazzese.ines@gmail.com>
2021-03-16 13:47:54 +0100

    fixed

--- server/src/routes/artist-auth-routes.js
+++ server/src/routes/artist-auth-routes.js
@@ -4,6 +4,5 @@ const adminController = require("../controllers/artist-auth.controller");
 
 router.post("/signup", adminController.signup);
 router.post("/login", adminController.login);
-// router.get("/verify", verifyToken);
 
 module.exports = router;
