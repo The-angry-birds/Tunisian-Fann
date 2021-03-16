bb35b771858ed331c53f6446a4d57575333a928b
ines <piazzese.ines@gmail.com>
2021-03-16 13:49:00 +0100

    fixed

--- server/src/routes/users-signup-router.js
+++ server/src/routes/users-signup-router.js
@@ -1,9 +1,5 @@
 const router = require("express").Router();
 const UsersControllerSignUp = require("../controllers/users-signup-contoller.js");
-
-
-
-
 router.post("/signup",UsersControllerSignUp.signup)
 router.post("/login",UsersControllerSignUp.login)
 
