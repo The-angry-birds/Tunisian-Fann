78c3463f07237e31acec2ce2d80747cdb6534989
Nabil-Elbir <nabil.elbir@gmail.com>
2021-03-17 20:25:43 +0100

    landing

---
+++ Users/src/components/LandingPage.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <NavBar></NavBar>
+    <AuctionCard></AuctionCard>
+    <ArtworkCard></ArtworkCard>
+  </div>
+</template>
+
+<script>
+import NavBar from "./NavBar.vue";
+import AuctionCard from "./AuctionCard.vue";
+import ArtworkCard from "./ArtworkCard.vue";
+
+// import axios from "axios";
+export default {
+  data() {
+    return {
+      // image: {},
+      // images: [],
+      // slide: 0,
+      // sliding: null,
+    };
+  },
+  // methods: {
+  //   displayImages() {
+  //     axios.get("http://localhost:3000/categorys").then(({ data }) => {
+  //       this.images = data.slice(1);
+  //       this.image = data[0];
+  //     });
+  //   },
+  // },
+  // mounted() {
+  //   this.displayImages();
+  // },
+
+  components: {
+    NavBar,
+    AuctionCard,
+    ArtworkCard,
+  },
+};
+</script>
+
+<style scoped>
+* {
+  font-family: "Spectral", serif;
+}
+</style>
+
