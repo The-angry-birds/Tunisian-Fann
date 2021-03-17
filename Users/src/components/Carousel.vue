97e694c2c1a7e009a8941ff80da7c03250a00f28
Nabil-Elbir <nabil.elbir@gmail.com>
2021-03-17 01:44:21 +0100

    started working on the auctions component

--- Users/src/components/Carousel.vue
+++ Users/src/components/Carousel.vue
@@ -1,15 +1,14 @@
 <template>
   <div>
-    <NavigationBar></NavigationBar>
-    <div class="carousel-container">
+    <NavBar></NavBar>
+    <div>
       <b-carousel
+        id="carousel-1"
         v-model="slide"
         :interval="0"
         controls
         fade
-        @sliding-start="onSlideStart"
-        @sliding-end="onSlideEnd"
-        id="carousel-1"
+        background="transparent"
         style="text-shadow: 1px 1px 2px #333"
       >
         <b-carousel-slide
@@ -18,6 +17,7 @@
           v-bind:text="image.description.slice(0, 50)"
           v-bind:img-src="image.ImageUrl"
         ></b-carousel-slide>
+
         <b-carousel-slide
           v-for="(item, index) in images"
           :key="index"
@@ -26,22 +26,23 @@
           v-bind:text="item.description"
           v-bind:img-src="item.ImageUrl"
         ></b-carousel-slide>
-      
       </b-carousel>
     </div>
+    <AuctionCard></AuctionCard>
   </div>
 </template>
 
 <script>
-import NavigationBar from "./NavigationBar.vue";
+import NavBar from "./NavBar.vue";
+import AuctionCard from "./AuctionCard.vue";
 import axios from "axios";
 export default {
   data() {
     return {
       image: {},
-      images: [], 
-       slide: 0,
-        sliding: null
+      images: [],
+      slide: 0,
+      sliding: null,
     };
   },
   methods: {
@@ -50,38 +51,40 @@ export default {
         this.images = data.slice(1);
         this.image = data[0];
       });
-      
     },
-    onSlideStart() {
-        this.sliding = true
-      },
-      onSlideEnd() {
-        this.sliding = false
-      }
   },
   mounted() {
-     
     this.displayImages();
   },
 
   components: {
-    NavigationBar,
+    NavBar,
+    AuctionCard,
   },
- 
-    
 };
 </script>
 
 <style scoped>
+* {
+  font-family: "Spectral", serif;
+}
+body
 .carousel-image {
-  width:100%;
-  max-height:92vh;
-  min-height:92vh;
+  width: 100%;
+  max-height: 70vh;
+  min-height: 70vh;
   object-fit: cover;
-  transition: 1s;
-
+  padding-left: 8px;
+  padding-right: 8px;
 }
 
+#carousel-1 {
+  width: 100%;
+  max-height: 70vh;
+  min-height: 70vh;
+  padding-left: 8px;
+  padding-right: 8px;
 
+}
 </style>
 
