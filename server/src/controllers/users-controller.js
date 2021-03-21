50e24bb1dd9f23d3518a16e00d3ab2a3355fb0e0
MalekWahmi <wahmimalek@gmail.com>
2021-03-21 19:39:05 +0100

    fix-schema-artist

--- server/src/controllers/users-controller.js
+++ server/src/controllers/users-controller.js
@@ -2,27 +2,26 @@
 const { User } = require("../../db/models/users-model-signup");
 const config = require("../../db/configUsers");
 const jwt = require("jsonwebtoken");
-// exports.getUser = async function (req, res) {
-//   try {
-//     const data = await User.findOne({ where: {token: req.params.token } });
-//     res.send(data);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
-exports.getUser = async (req, res) => {
+exports.getUser = async function (req, res) {
   try {
-    // console.log("=====>", req.headers);
-    const token = req.headers.authorization.split(" ")[1];
-    const email = await jwt.verify(token, config.secret);
-    const user = await User.findOne({
-      where: { email: email.email },
-    });
-    console.log("user ===>", user.dataValues);
-    res.send(user.dataValues);
+    const data = await User.findAll({});
+    res.send(data);
   } catch (err) {
-    res.send(err);
+    console.log(err);
   }
 };
+// exports.getUser = async (req, res) => {
+//   try {
+//     // console.log("=====>", req.headers);
+//     const token = req.headers.authorization.split(" ")[1];
+//     const email = await jwt.verify(token, config.secret);
+//     const user = await User.findOne({
+//       where: { email: email.email },
+//     });
+//     console.log("user ===>", user.dataValues);
+//     res.send(user.dataValues);
+//   } catch (err) {
+//     res.send(err);
+//   }
+// };
 
- 
