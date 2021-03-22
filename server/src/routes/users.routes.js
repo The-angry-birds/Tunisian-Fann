02fbce71a69ed00087961953024184b876eabfc4
jdidi94 <jdididaoud404@gmail.com>
2021-03-22 22:12:20 +0100

    new card

--- server/src/routes/users.routes.js
+++ server/src/routes/users.routes.js
@@ -1,7 +1,10 @@
 const router = require("express").Router();
 const controllerUsers = require("../controllers/users-controller.js");
 
+
 router.get('/',controllerUsers.getUser)
+router.patch('/:id',controllerUsers.editUser)
+router.get('/getUserByToken',controllerUsers.getUserByToken)
 
 
 module.exports = router;
