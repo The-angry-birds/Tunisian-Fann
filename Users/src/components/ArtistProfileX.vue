e4fbca7dee8a7a440c9f6e56a6bdbef52aa34d17
Nabil-Elbir <nabil.elbir@gmail.com>
2021-03-23 15:09:09 +0100

    more fixes on profil and fetched real data

--- Users/src/components/ArtistProfileX.vue
+++ Users/src/components/ArtistProfileX.vue
@@ -4,24 +4,21 @@
     <div>
       <div class="row py-8 px-8">
         <div class="col-md-25 mx-auto">
-          <!-- Profile widget -->
           <div class="bg-white shadow rounded overflow-hidden">
             <div class="px-4 pt-0 pb-4 cover">
               <div class="media align-items-end profile-head">
                 <div class="profile mr-3">
                   <img
-                    src="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80"
+                    src="https://i.ibb.co/TkxhgdJ/nabil-elbir.jpg"
                     alt="#"
                     width="130"
                     class="rounded mb-2 img-thumbnail"
-                  /><a href="#" class="btn btn-outline-dark btn-sm btn-block"
-                    >Edit profile</a
-                  >
+                  />
                 </div>
                 <div class="media-body mb-5 text-black">
-                  <h4 class="mt-0 mb-0">Mark Williams</h4>
+                  <h4 class="mt-0 mb-0">Nabil Elbir</h4>
                   <p class="small mb-4">
-                    <i class="fas fa-map-marker-alt mr-2"></i>New York
+                    <i class="fas fa-map-marker-alt mr-2"></i>Nabeul
                   </p>
                 </div>
               </div>
@@ -33,22 +30,23 @@
             <div class="px-3 py-3">
               <h5 class="mb-3">About</h5>
               <div class="p-4 rounded shadow-sm" id="info-card">
-                <p class="font-italic mb-0">Web Developer</p>
-                <p class="font-italic mb-0">Lives in New York</p>
-                <p class="font-italic mb-0">Photographer</p>
+                <p class="font-italic mb-0">Related text</p>
+                <p class="font-italic mb-0">Related text</p>
+                <p class="font-italic mb-0">Related text</p>
               </div>
             </div>
             <div class="py-3 px-3">
               <h5 class="mb-3">Artworks</h5>
-              <div class="p-4 rounded shadow-sm" id="info-card">
+              <div
+                class="p-4 rounded shadow-sm"
+                id="artworks-cards"
+                v-for="(artwork, i) in artworks"
+                :key="i"
+              >
                 <div class="artwork-card">
-                  <img
-                    class="card-img"
-                    src="https://www.bensalemwalid.com/wp-content/uploads/2021/02/cry-baby-artwork-by-bensalem-walid.png"
-                    alt="#"
-                  />
-                  <h3>Cry Baby</h3>
-                  <p>Digital Paitings</p>
+                  <img class="card-img" v-bind:src="artwork.imageUrl" alt="#" />
+                  <h3>{{ artwork.nameArtwork }}</h3>
+                  <p>Digital Paintings</p>
                   <p>320 DT</p>
                 </div>
               </div>
@@ -61,16 +59,32 @@
 </template>
 <script>
 import NavBar from "./NavBar.vue";
+import axios from "axios";
 export default {
   data() {
     return {
-      data: [],
+      artworks: [],
     };
   },
   components: {
     NavBar,
   },
-  methods: {},
+  methods: {
+    getArtworks() {
+      axios
+        .get(`http://localhost:3000/api/artworks`)
+        .then((artworks) => {
+          this.artworks = artworks.data;
+          console.log(artworks.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+  mounted() {
+    this.getArtworks();
+  },
 };
 </script>
 <style scoped>
@@ -94,19 +108,34 @@ export default {
   background-color: #fdf5e6;
 }
 
-.artwork-card {
-  max-width: 240px;
-  min-width: 240px;
-  max-height: 400px;
-  min-height: 400px;
-  border: solid 1px black;
-  
+#artworks-cards {
+  background-color: #fdf5e6;
+  display: flex;
+  flex-wrap: wrap;
+  /* justify-content: center; */
 }
+
+.artwork-card {
+  width: 300px;
+  max-height: 380px;
+  min-height: 380px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  border-radius: 5px;
+  text-align: center;
+  margin: 10px;
+  box-sizing: border-box;
+}
+.artwork-card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+}
+
 .card-img {
   width: 100%;
   max-height: 240px;
   min-height: 240px;
   object-fit: cover;
-  
+  border-radius: 5px 5px 0px 0px;
+  margin-bottom: 15px;
 }
 </style>
