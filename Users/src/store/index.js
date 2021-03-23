01042adac983227059fba68fe9990c02939dfae2
ines <piazzese.ines@gmail.com>
2021-03-23 19:06:38 +0100

    ines

--- Users/src/store/index.js
+++ Users/src/store/index.js
@@ -4,15 +4,11 @@ import axios from "axios";
 import artist from "./modules/artist.js";
 import user from "./modules/user.js";
 
-import axios from "axios";
-import swal from "sweetalert";
-
-
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
     artist,
     user,
-  }})
-
+  },
+});
