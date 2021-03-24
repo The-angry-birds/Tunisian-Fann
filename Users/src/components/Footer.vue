c64b106d936eb79dd10021530d7f9fd888a07afc
Nabil-Elbir <nabil.elbir@gmail.com>
2021-03-24 15:19:35 +0100

    fixed footer layout

--- Users/src/components/Footer.vue
+++ Users/src/components/Footer.vue
@@ -1,188 +1,149 @@
 <template>
-  <footer id="fh5co-footer" class="fh5co-bg" role="contentinfo">
-    <div class="overlay"></div>
+  <section id="footer">
     <div class="container">
-      <div class="row row-pb-md">
-        <div class="col-md-4 fh5co-widget">
-          <h3>A Little About Tunisian Fann</h3>
-          <p>
-            Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia
-            reprehenderit. Eos cumque dicta adipisci architecto culpa amet.
-          </p>
-          <p><a class="btn" href="#">Join us</a></p>
+      <div class="row text-center text-xs-center text-sm-left text-md-left">
+        <div class="col-xs-12 col-sm-4 col-md-4">
+          <h5>Tunisian Fann</h5>
         </div>
-        <div class="icons col-md-8">
-          <h3 class="">Reach us</h3>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="30"
-            height="30"
-            fill="currentColor"
-            class="bi bi-google"
-            viewBox="0 0 16 16"
-          >
-            <path
-              d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z"
-            />
-          </svg>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="30"
-            height="30"
-            fill="currentColor"
-            class="bi bi-facebook"
-            viewBox="0 0 16 16"
-          >
-            <path
-              d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"
-            />
-          </svg>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="30"
-            height="30"
-            fill="currentColor"
-            class="bi bi-instagram"
-            viewBox="0 0 16 16"
-          >
-            <path
-              d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"
-            />
-          </svg>
+        <div class="col-xs-12 col-sm-4 col-md-4">
+          <h5>Services</h5>
+          <ul class="list-unstyled quick-links">
+            <li>
+              <a href="#"><i class="fa fa-angle-double-right"></i>Auctions</a>
+            </li>
+            <li>
+              <a href="#"><i class="fa fa-angle-double-right"></i>Artworks</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-xs-12 col-sm-4 col-md-4">
+          <h5>Quick links</h5>
+          <ul class="list-unstyled quick-links">
+            <li>
+              <a href="#"><i class="fa fa-angle-double-right"></i>Join Us</a>
+            </li>
+            <li>
+              <a href="#"><i class="fa fa-angle-double-right"></i>About Us</a>
+            </li>
+          </ul>
         </div>
       </div>
-
-      <div class="row copyright">
-        <div class="col-md-12 text-center">
-          <p>
-            <small class="block">&copy; 2021 | All Rights Reserved.</small>
-            <small class="block">Powered by The Angry Birds</small>
-          </p>
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
+          <ul class="list-unstyled list-inline social text-center">
+            <li class="list-inline-item">
+              <a href="#"><i class="fa fa-facebook"></i></a>
+            </li>
+            <li class="list-inline-item">
+              <a href="#"><i class="fa fa-twitter"></i></a>
+            </li>
+            <li class="list-inline-item">
+              <a href="#"><i class="fa fa-instagram"></i></a>
+            </li>
+            <li class="list-inline-item">
+              <a href="#"><i class="fa fa-google-plus"></i></a>
+            </li>
+            <li class="list-inline-item">
+              <a href="#" target="_blank"><i class="fa fa-envelope"></i></a>
+            </li>
+          </ul>
         </div>
+        <hr />
+      </div>
+      <div class="row">
+        <div
+          class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white"
+        >
+          <p>
+            <u
+              ><a href="https://github.com/The-angry-birds"
+                >The Angry Birds</a
+              ></u
+            >
+          </p>
+          <p class="h6" style="color: black">© All rights reserved.</p>
+        </div>
+        <hr />
       </div>
     </div>
-  </footer>
+  </section>
 </template>
-
 <script>
 export default {};
 </script>
 
 <style scoped>
-* {
-  font-family: "Spectral", serif;
+@import url("https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
+section {
+  margin-top: 30px;
+  padding-top: 60px;
 }
-
-.bi {
-  margin-right: 10px;
-}
-
-.fh5co-bg {
-  background-size: cover;
-  background-position: center center;
-  background-repeat: no-repeat;
-  position: relative;
-}
-
-.fh5co-bg {
-  background-size: cover;
-  background-position: center center;
-  position: relative;
-  width: 100%;
-  float: left;
-}
-
-.fh5co-bg .overlay {
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background: rgba(0, 0, 0, 0.5);
-  -webkit-transition: 0.5s;
-  -o-transition: 0.5s;
-  transition: 0.5s;
-}
-
-#fh5co-footer {
-  margin-top: 2em;
-  padding-top: 2em;
-}
-
-@media screen and (max-width: 768px) {
-  #fh5co-footer {
-    padding: 3em 0;
-  }
-}
-
-#fh5co-footer {
-  position: relative;
-}
-
-#fh5co-footer .overlay {
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background: rgb(240, 240, 240);
-  -webkit-transition: 0.5s;
-  -o-transition: 0.5s;
-  transition: 0.5s;
-}
-
-#fh5co-footer h3 {
-  margin-bottom: 15px;
-  font-weight: bold;
-  font-size: 15px;
-  letter-spacing: 2px;
-  text-transform: uppercase;
-  color: black;
-}
-
-#fh5co-footer .fh5co-footer-links {
-  padding: 0;
-  margin: 0;
-}
-
-#fh5co-footer .fh5co-footer-links li {
-  padding: 0;
-  margin: 0;
-  list-style: none;
-}
-
-#fh5co-footer .fh5co-footer-links li a {
-  color: black;
-  text-decoration: none;
-}
-
-#fh5co-footer .fh5co-footer-links li a:hover {
-  text-decoration: underline;
-}
-
-@media screen and (max-width: 768px) {
-  #fh5co-footer .fh5co-widget {
-    text-align: left;
-  }
-}
-
-#fh5co-footer .fh5co-widget h3 {
-  margin-bottom: 15px;
-  font-weight: bold;
-  font-size: 15px;
-  letter-spacing: 2px;
+section .section-title {
+  text-align: center;
+  color: #000000;
   text-transform: uppercase;
 }
-
-#fh5co-footer .copyright .block {
-  display: block;
+.row {
+  margin-top: -20px;
 }
-
-.btn {
-  color: rgb(0, 0, 0);
+#footer {
+  background: rgb(255, 255, 255) !important;
+  box-shadow: 0px 4px 5px #888, 0px -4px 5px #888;
 }
-
-.classes-title {
-  margin-left: 15px;
+#footer h5 {
+  padding-left: 10px;
+  border-left: 3px solid black;
+  color: black;
+}
+#footer a {
+  color: #000000;
+  text-decoration: none !important;
+  background-color: transparent;
+  -webkit-text-decoration-skip: objects;
+}
+#footer ul.social li a i {
+  margin-right: 5px;
+  font-size: 25px;
+  -webkit-transition: 0.5s all ease;
+  -moz-transition: 0.5s all ease;
+  transition: 0.5s all ease;
+}
+#footer ul.social li:hover a i {
+  color: #ad7d52;
+  padding-right: 25px;
+  padding-left: 25px;
+}
+#footer ul.social li a,
+#footer ul.quick-links li a {
+  color: #000000;
+}
+#footer ul.social li a:hover {
+  color: #000000;
+}
+#footer ul.quick-links li {
+  padding: 3px 0;
+  -webkit-transition: 0.5s all ease;
+  -moz-transition: 0.5s all ease;
+  transition: 0.5s all ease;
+}
+#footer ul.quick-links li:hover {
+  padding: 3px 0;
+  margin-left: 5px;
+  font-weight: 700;
+}
+#footer ul.quick-links li a i {
+  margin-right: 5px;
+}
+#footer ul.quick-links li:hover a i {
+  font-weight: 700;
+  color: black;
+}
+@media (max-width: 767px) {
+  #footer h5 {
+    padding-left: 0;
+    border-left: transparent;
+    padding-bottom: 0px;
+    margin-bottom: 10px;
+  }
 }
 </style>
