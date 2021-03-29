4f4e71c62587528899968c5e656af5c949a136e2
MalekWahmi <wahmimalek@gmail.com>
2021-03-30 00:23:09 +0100

    payment-staging

--- Users/src/components/CardInformation.vue
+++ Users/src/components/CardInformation.vue
@@ -1,245 +1,233 @@
 <template>
-  <div>
- <h1>ddddddddddddddd</h1>
-  <div class="d-flex flex-column align-items-center">
-    <h2 class="text-primary">{{msg}} {{payAmount}}</h2>
-    <form id="payment-form" class="w-75 px-5 d-flex flex-column align-items-center" >
-        <div ref="card" class="form-control m-2">
-          <!-- A Stripe Element will be inserted here. -->
+  <div class="payment">
+    <div class="container-fluid">
+      <div class="row justify-content-center">
+        <div class="card my-4 p-3">
+          <div class="row main">
+            <div class="col-12">
+              <span>Cart</span>&nbsp;&nbsp;&nbsp;&nbsp;<span
+                >Shipping confirmation</span
+              >&nbsp;&nbsp;&nbsp;&nbsp;<span>Credit card checkout</span>
+            </div>
+          </div>
+          <div class="row justify-content-center mrow">
+            <div class="col-12">
+              <img
+                src="https://img.icons8.com/color/48/000000/mastercard-logo.png"
+                width="35px"
+                height="35px"
+              />
+              <img
+                src="https://img.icons8.com/color/48/000000/visa.png"
+                width="35px"
+                height="35px"
+              />
+            </div>
+          </div>
+          <form class="form-card">
+            <div class="row">
+              <div class="col-12">
+                <div class="form-group">
+                  <input type="text" class="form-control p-0" />
+                  <label class="form-control-placeholder p-0" for="number"
+                    >CardNumber</label
+                  >
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-12">
+                <div class="form-group">
+                  <input
+                    type="text"
+                    class="form-control p-0"
+                    id="name"
+                    required
+                  />
+                  <label class="form-control-placeholder p-0" for="email"
+                    >Email</label
+                  >
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-sm-4 col-12">
+                <div class="form-group">
+                  <input
+                    type="text"
+                    class="form-control p-0"
+                    id="expdate"
+                    required
+                  placeholder="MM/YY"
+                  />
+                  <label class="form-control-placeholder p-0" for="expdate"
+                    >ExpirationDate</label
+                  >
+                </div>
+              </div>
+              <div class="col-sm-4 col-12">
+                <div class="form-group">
+                  <input
+                    type="password"
+                    class="form-control p-0"
+                    id="passw"
+                    required
+                    v-model="cvc"
+                    placeholder="CVC"
+                  />
+                  <label class="form-control-placeholder p-0" for="passw"
+                    >CVV</label
+                  >
+                </div>
+              </div>
+            </div>
+            <div class="row lrow mt-4 mb-3">
+              <div class="col-sm-8 col-12">
+                <h3>Grand Total:</h3>
+              </div>
+              <div class="col-sm-4 col-12">
+                <h5>&#36;1,449.00</h5>
+              </div>
+            </div>
+            <div class="row mb-2">
+              <div class="col-sm-12">
+                <button
+                  type="button"
+                  class="btn btn-primary btn-block"
+                  @click.prevent="handleSubmit()"
+                >
+                  Pay
+                </button>
+              </div>
+            </div>
+          </form>
         </div>
-        <input :disabled="lockSubmit" class="btn btn-primary shadow-sm" type="submit" value="Donate" v-on:click.prevent="purchase" />
-    </form>
+      </div>
+    </div>
+
   </div>
-  </div>
+
 </template>
 <script>
-import Stripe from 'stripe'
-import axios from 'axios'
+import axios from "axios";
+
 export default {
+  
   data() {
     return {
-      spk: "pk_test_51ISo8BBdTsia79TvvPiOhcEdgJW5RrLgYuqSmgo7FwC1JdsSIeCd0O867AdapRzapwhZYY8mCfrbqK9yHTh3LPxf00pIAjIYFw",
-      stripe: undefined,
-      card: undefined,
-      msg: "Donate Here",
-      payAmount: "$10.00",
-      lockSubmit: false,
-      api: "http://localhost:3000/payment",
-    };
-  },
-    mounted: function () {
-    var self=this;
-    self.stripe= Stripe(self.spk);
-    self.card = self.stripe.elements().create('card');
-    self.card.mount(self.$refs.card);
-   },
-   methods:{
-    purchase() {
-      var self = this;
-      self.lockSubmit=true
+     cardumber:"",
+     cvc :"",
 
-      self.stripe.createToken(self.cardNumber).then(function(result) {
-        if (result.error) {
-          alert(result.error.message)
-          self.$forceUpdate(); // Forcing the DOM to update so the Stripe Element can update.
-          self.lockSubmit=false
-          return;
-        }
-        else{ 
-          self.processTransaction(result.token.id)
-        }
-      })
-      .catch((err) => {
-        alert("error: "+err.message)
-        self.lockSubmit=false
-      });
-    },
-     processTransaction(transactionToken){
-      var self=this;
-     var dt= {
-          amount:self.stripCurrency(self.payAmount), //stripe uses an int [with shifted decimal place] so we must convert our payment amount
-          currency:"USD",
-          description:"something to say",
-          token:transactionToken
+       
+
       }
-      var route=self.api+"/charge/card"
-      axios.post(route,dt, {
-          headers: {
-          }
-      }).then(response => {
-          if(response.status==200){
-              alert("Transaction succeeded")
-              self.lockSubmit=false
-          }
-          else{
-              throw new Error("failed donation")
-          }
-      }).catch((err) => {
-          alert("error: "+err.message)
-          self.lockSubmit=false
+    },
+
+  methods: {
+    handleSubmit() {
+      const payment = {
+        adress: {
+          city: this.city,
+          country: this.country,
+          line1: this.line1,
+          line2: this.line2,
+          postal_code: this.postal_code,
+          state: this.state,
+        },
+        email: this.email,
+        name: this.name,
+        phone: this.phone,
+      };
+      axios.post("http://localhost:3000/v1/charges", payment).then((res) => {
+        console.log("==========>", res);
       });
     },
-    stripCurrency(val){
-      return val.replace(',','').relpace('$','').replace('.','')
-    }
-    
-   }
-}
-
-
-// <script>
-// import axios from "axios"
-// export default {
-//   data() {
-//     return {
-//       adress:{
-//       city: "",
-//       country: "",
-//       line1: "",
-//       line2: "",
-//       postal_code: "",
-//       state: "",
-//       },
-//       email: "",
-//       name: "",
-//       phone: "",
-//       Creditcardnumber:"",
-//       exp_month: "",
-//       exp_year: "",
-//     };
-//   },
-//   methods: {
-//     handleSubmit() {
-//       const payment ={
-//         adress:{
-//       city: this.city,
-//       country: this.country,
-//       line1: this.line1,
-//       line2: this.line2,
-//       postal_code: this.postal_code,
-//       state: this.state,
-//         },
-//       email: this.email,
-//       name: this.name,
-//       phone: this.phone,
-
-//       }
-//       axios.post("http://localhost:3000/v1/charges",payment)
-//       .then((res)=>{console.log(res);})
-//     }
-
-//   },
-// }
-//
+  },
+};
 </script>
 
 <style  scoped>
 body {
-  font-family: Arial;
-  font-size: 17px;
-  padding: 8px;
+  font-family: Arial, Helvetica, sans-serif;
+  background: #343a40;
+}
+.payment{
+  margin-top: 10%;
 }
 
-* {
-  box-sizing: border-box;
+.card {
+  background: #000;
+  color: #fff;
+  width: 410px !important;
 }
 
-.row {
-  display: -ms-flexbox; /* IE10 */
-  display: flex;
-  -ms-flex-wrap: wrap; /* IE10 */
-  flex-wrap: wrap;
-  margin: 0 -16px;
-  margin-top: 120px;
-  margin-left: 120px;
+.mrow {
+  margin-top: 30px;
+  margin-bottom: 30px;
 }
 
-.col-25 {
-  -ms-flex: 25%; /* IE10 */
-  flex: 25%;
+img {
+  margin-right: 10px;
 }
 
-.col-50 {
-  -ms-flex: 50%; /* IE10 */
-  flex: 50%;
-}
-
-.col-75 {
-  -ms-flex: 75%; /* IE10 */
-  flex: 75%;
-}
-
-.col-25,
-.col-50,
-.col-75 {
-  padding: 0 16px;
-}
-
-.container {
-  background-color: white;
-  box-shadow: 10px 10px 10px;
-  padding: 5px 20px 15px 20px;
-  border: 1px solid lightgrey;
-  border-radius: 3px;
-}
-
-input[type="text"] {
-  width: 100%;
-  margin-bottom: 20px;
-  padding: 12px;
-  border: 1px solid #ccc;
-  border-radius: 3px;
-}
-
-label {
-  margin-bottom: 10px;
-  display: block;
-}
-
-.icon-container {
-  margin-bottom: 20px;
-  padding: 7px 0;
-  font-size: 24px;
-}
-
-.btn {
-  background-color: grey;
-  color: white;
-  padding: 12px;
-  margin: 10px 0;
-  border: none;
-  width: 100%;
-  border-radius: 3px;
+.main span:hover {
+  text-decoration: underline;
   cursor: pointer;
-  font-size: 17px;
-  transition: 0.5s;
 }
 
-.btn:hover {
-  background-color: white;
-  color: grey;
+.mrow img:hover {
+  border-bottom: 1px solid #fff;
+  cursor: pointer;
 }
 
-a {
-  color: #2196f3;
+.btn-primary {
+  border: none;
+  border-radius: 30px;
 }
 
-hr {
-  border: 1px solid lightgrey;
+h5 {
+  padding-top: 8px;
 }
 
-span.price {
-  float: right;
-  color: grey;
+.form-group {
+  position: relative;
+  margin-bottom: 2rem;
 }
 
-/* Responsive layout - when the screen is less than 800px wide, make the two columns stack on top of each other instead of next to each other (also change the direction - make the "cart" column go on top) */
-@media (max-width: 800px) {
-  .row {
-    flex-direction: column-reverse;
-  }
-  .col-25 {
-    margin-bottom: 20px;
-  }
+.form-control-placeholder {
+  position: absolute;
+  top: 6px;
+  padding: 7px 0 0 10px;
+  transition: all 200ms;
+  opacity: 0.5;
+  color: #dae0e5 !important;
+  font-size: 75%;
+}
+
+.form-control:focus + .form-control-placeholder,
+.form-control:valid + .form-control-placeholder {
+  font-size: 75%;
+  transform: translate3d(0, -100%, 0);
+  opacity: 1;
+  top: 10px;
+}
+
+.form-control {
+  background: transparent;
+  border: none;
+  border-bottom: 1px solid #fff !important;
+  border-radius: 0;
+  outline: 0;
+}
+
+.form-control:focus,
+.form-control:after {
+  outline-width: 0;
+  border-bottom: 1px solid #fff !important;
+  background: transparent;
+  box-shadow: none;
+  border-radius: 0;
+  color: #dae0e5;
+  letter-spacing: 1px;
 }
 </style>
--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,17 @@
 require("dotenv").config();
-
 const express = require("express");
-const bodyParser = require("body-parser");
+const bodyparser = require("body-parser");
+const port = process.env.PORT || 3000;
+const path = require("path");
+const app = express();
+
 const cors = require("cors");
 const morgan = require("morgan");
 const router = require("./routes/admin.routes.js");
 const adminRoutes = require("./routes/auth.admin.routes.js");
-const port = process.env.PORT || 3000;
-const path = require("path");
+
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
 
 const usersRoutes = require("./routes/users.routes.js");
 const usersSignupRoutes = require("./routes/auth.users.routes.js");
@@ -17,10 +21,8 @@ const artworkRouter = require("./routes/artwork-routes");
 const auctionsRouter = require("./routes/auctions.routes");
 const likesRouter = require("./routes/routes.likes");
 const verifyRouter = require("./routes/auth.verify.routes");
-const app = express();
 
 app.use(morgan("combined"));
-app.use(bodyParser.json());
 app.use(cors());
 morgan(":method :url :status :res[content-length] - :response-time ms");
 app.use("/api/auth/admin", adminRoutes);
@@ -35,55 +37,6 @@ app.use("/api/artists", artistRoutes);
 app.use("/api/auctions", auctionsRouter);
 app.use("/api/auth", verifyRouter);
 app.use("/api/likes", likesRouter);
-// View Engine Setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
-var Publishable_Key = process.env.Publishable_Key;
-var Secret_Key = process.env.Secret_Key;
-
-const stripe = require("stripe")(Secret_Key);
-
-
-app.get("/", function (req, res) {
-  res.render("Home", {
-    key: Publishable_Key,
-  });
-});
-
-app.post('/payment', function(req, res){ 
-
-    // Moreover you can take more details from user 
-    // like Address, Name, etc from form 
-    stripe.customers.create({ 
-        email: req.body.stripeEmail, 
-        source: req.body.stripeToken, 
-        name: 'Gautam Sharma', 
-        address: { 
-            line1: 'TC 9/4 Old MES colony', 
-            postal_code: '110092', 
-            city: 'New Delhi', 
-            state: 'Delhi', 
-            country: 'India', 
-        } 
-    }) 
-    .then((customer) => { 
-
-        return stripe.charges.create({ 
-            amount: 7000,    // Charing Rs 25 
-            description: 'Web Development Product', 
-            currency: 'USD', 
-            customer: customer.id 
-        }); 
-    }) 
-    .then((charge) => { 
-        res.send("Success") // If no error occurs 
-    }) 
-    .catch((err) => { 
-        res.send(err)    // If some error occurs 
-    }); 
-}) 
-
 
 // app.post("/sendmessage", (req, res) => {
 //   console.log(req.body);
@@ -101,6 +54,52 @@ app.post('/payment', function(req, res){
 //     .then((message) => res.send(message));
 // });
 
+var Publishable_Key = process.env.Publishable_Key;
+var Secret_Key = process.env.Secret_Key;
+
+const stripe = require("stripe")(Secret_Key);
+
+// View Engine Setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+  res.render("Home", {
+    key: Publishable_Key,
+  });
+});
+
+app.post("/payment", function (req, res) {
+  // Moreover you can take more details from user
+  // like Address, Name, etc from form
+  stripe.customers
+    .create({
+      email: req.body.stripeEmail,
+      source: req.body.stripeToken,
+      name: req.body.name,
+      address: {
+        line1: req.body.line1,
+        postal_code: req.body.line2,
+        city: req.body.city,
+        state: req.body.state,
+        country: req.body.state,
+      },
+    })
+    .then((customer) => {
+      return stripe.charges.create({
+        amount: 7000, // Charing Rs 25
+        description: "Web Development Product",
+        currency: "USD",
+        customer: customer.id,
+      });
+    })
+    .then((charge) => {
+      res.send("Success"); // If no error occurs
+    })
+    .catch((err) => {
+      res.send(err); // If some error occurs
+    });
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("listening on port 3000");
