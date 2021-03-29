f29a8567defbd1d2a9605e4e6d105d62c90b71d0
MalekWahmi <wahmimalek@gmail.com>
2021-03-29 22:42:45 +0100

    payment-front

--- Users/src/components/CardInformation.vue
+++ Users/src/components/CardInformation.vue
@@ -1,84 +1,136 @@
 <template>
   <div>
-   
-<div class="row">
-  <div class="col-75">
-    <div class="container">
-      <form action="/action_page.php">
-      
-        <div class="row">
-          <div class="col-50">
-            <h3>Billing Address</h3>
-            <label for="fname"><i class="fa fa-user"></i> Full Name</label>
-            <input type="text" id="fname" name="firstname" placeholder="John M. Doe">
-         
-            <label for="adr"><i class="fa fa-address-card-o"></i> Address</label>
-            <input type="text" id="adr" name="address" placeholder="542 W. 15th Street">
-            <label for="city"><i class="fa fa-institution"></i> City</label>
-            <input type="text" id="city" name="city" placeholder="New York">
-
-            <div class="row">
-              <div class="col-50">
-                <label for="state">State</label>
-                <input type="text" id="state" name="state" placeholder="NY">
-              </div>
-              <div class="col-50">
-                <label for="zip">Zip</label>
-                <input type="text" id="zip" name="zip" placeholder="10001">
-              </div>
-            </div>
-          </div>
-
-          <div class="col-50">
-            <h3>Payment</h3>
-            <label for="fname">Accepted Cards</label>
-            <div class="icon-container">
-              <i class="fa fa-cc-visa" style="color:navy;"></i>
-              <i class="fa fa-cc-amex" style="color:blue;"></i>
-              <i class="fa fa-cc-mastercard" style="color:red;"></i>
-              <i class="fa fa-cc-discover" style="color:orange;"></i>
-            </div>
-            <label for="cname">Name on Card</label>
-            <input type="text" id="cname" name="cardname" placeholder="John More Doe">
-            <label for="ccnum">Credit card number</label>
-            <input type="text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444">
-            <label for="expmonth">Exp Month</label>
-            <input type="text" id="expmonth" name="expmonth" placeholder="September">
-            <div class="row">
-              <div class="col-50">
-                <label for="expyear">Exp Year</label>
-                <input type="text" id="expyear" name="expyear" placeholder="2018">
-              </div>
-              <div class="col-50">
-                <label for="cvv">CVV</label>
-                <input type="text" id="cvv" name="cvv" placeholder="352">
-              </div>
-            </div>
-          </div>
-          
+ <h1>ddddddddddddddd</h1>
+  <div class="d-flex flex-column align-items-center">
+    <h2 class="text-primary">{{msg}} {{payAmount}}</h2>
+    <form id="payment-form" class="w-75 px-5 d-flex flex-column align-items-center" >
+        <div ref="card" class="form-control m-2">
+          <!-- A Stripe Element will be inserted here. -->
         </div>
-        <label>
-          <input type="checkbox" checked="checked" name="sameadr"> Shipping address same as billing
-        </label>
-        <input type="submit" value="Continue to checkout" class="btn">
-      </form>
-    </div>
+        <input :disabled="lockSubmit" class="btn btn-primary shadow-sm" type="submit" value="Donate" v-on:click.prevent="purchase" />
+    </form>
   </div>
-  <div class="col-25">
-  
-  </div>
-</div>
-
-
   </div>
 </template>
-
 <script>
-
-
-
+import Stripe from 'stripe'
+import axios from 'axios'
 export default {
+  data() {
+    return {
+      spk: "pk_test_51ISo8BBdTsia79TvvPiOhcEdgJW5RrLgYuqSmgo7FwC1JdsSIeCd0O867AdapRzapwhZYY8mCfrbqK9yHTh3LPxf00pIAjIYFw",
+      stripe: undefined,
+      card: undefined,
+      msg: "Donate Here",
+      payAmount: "$10.00",
+      lockSubmit: false,
+      api: "http://localhost:3000/payment",
+    };
+  },
+    mounted: function () {
+    var self=this;
+    self.stripe= Stripe(self.spk);
+    self.card = self.stripe.elements().create('card');
+    self.card.mount(self.$refs.card);
+   },
+   methods:{
+    purchase() {
+      var self = this;
+      self.lockSubmit=true
+
+      self.stripe.createToken(self.cardNumber).then(function(result) {
+        if (result.error) {
+          alert(result.error.message)
+          self.$forceUpdate(); // Forcing the DOM to update so the Stripe Element can update.
+          self.lockSubmit=false
+          return;
+        }
+        else{ 
+          self.processTransaction(result.token.id)
+        }
+      })
+      .catch((err) => {
+        alert("error: "+err.message)
+        self.lockSubmit=false
+      });
+    },
+     processTransaction(transactionToken){
+      var self=this;
+     var dt= {
+          amount:self.stripCurrency(self.payAmount), //stripe uses an int [with shifted decimal place] so we must convert our payment amount
+          currency:"USD",
+          description:"something to say",
+          token:transactionToken
+      }
+      var route=self.api+"/charge/card"
+      axios.post(route,dt, {
+          headers: {
+          }
+      }).then(response => {
+          if(response.status==200){
+              alert("Transaction succeeded")
+              self.lockSubmit=false
+          }
+          else{
+              throw new Error("failed donation")
+          }
+      }).catch((err) => {
+          alert("error: "+err.message)
+          self.lockSubmit=false
+      });
+    },
+    stripCurrency(val){
+      return val.replace(',','').relpace('$','').replace('.','')
+    }
+    
+   }
 }
+
+
+// <script>
+// import axios from "axios"
+// export default {
+//   data() {
+//     return {
+//       adress:{
+//       city: "",
+//       country: "",
+//       line1: "",
+//       line2: "",
+//       postal_code: "",
+//       state: "",
+//       },
+//       email: "",
+//       name: "",
+//       phone: "",
+//       Creditcardnumber:"",
+//       exp_month: "",
+//       exp_year: "",
+//     };
+//   },
+//   methods: {
+//     handleSubmit() {
+//       const payment ={
+//         adress:{
+//       city: this.city,
+//       country: this.country,
+//       line1: this.line1,
+//       line2: this.line2,
+//       postal_code: this.postal_code,
+//       state: this.state,
+//         },
+//       email: this.email,
+//       name: this.name,
+//       phone: this.phone,
+
+//       }
+//       axios.post("http://localhost:3000/v1/charges",payment)
+//       .then((res)=>{console.log(res);})
+//     }
+
+//   },
+// }
+//
 </script>
 
 <style  scoped>
@@ -86,7 +138,6 @@ body {
   font-family: Arial;
   font-size: 17px;
   padding: 8px;
- 
 }
 
 * {
@@ -99,10 +150,8 @@ body {
   -ms-flex-wrap: wrap; /* IE10 */
   flex-wrap: wrap;
   margin: 0 -16px;
-  margin-top:120px;
+  margin-top: 120px;
   margin-left: 120px;
- 
-  
 }
 
 .col-25 {
@@ -127,14 +176,14 @@ body {
 }
 
 .container {
-  background-color:white;
-  box-shadow:10px 10px 10px ;
+  background-color: white;
+  box-shadow: 10px 10px 10px;
   padding: 5px 20px 15px 20px;
   border: 1px solid lightgrey;
   border-radius: 3px;
 }
 
-input[type=text] {
+input[type="text"] {
   width: 100%;
   margin-bottom: 20px;
   padding: 12px;
@@ -154,7 +203,7 @@ label {
 }
 
 .btn {
-  background-color:grey;
+  background-color: grey;
   color: white;
   padding: 12px;
   margin: 10px 0;
@@ -163,16 +212,16 @@ label {
   border-radius: 3px;
   cursor: pointer;
   font-size: 17px;
-  transition:0.5s
+  transition: 0.5s;
 }
 
 .btn:hover {
   background-color: white;
-  color:grey
+  color: grey;
 }
 
 a {
-  color: #2196F3;
+  color: #2196f3;
 }
 
 hr {
