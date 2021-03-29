4949ffc3fe69b42902eed96182794edf80464f43
Nabil-Elbir <nabil.elbir@gmail.com>
2021-03-29 13:01:02 +0100

    started working on the artist profile

--- Users/src/components/ArtistProfileView.vue
+++ Users/src/components/ArtistProfileView.vue
@@ -1,53 +1,100 @@
 <template>
   <div>
-    <NavBar></NavBar>
-    <div>
-      <div class="row py-8 px-8">
-        <div class="col-md-25 mx-auto">
-          <div class="bg-white shadow rounded overflow-hidden">
-            <div class="px-4 pt-0 pb-4 cover">
-              <div class="media align-items-end profile-head">
-                <div class="profile mr-3">
-                  <img
-                    src="https://i.ibb.co/TkxhgdJ/nabil-elbir.jpg"
-                    alt="#"
-                    width="130"
-                    class="rounded mb-2 img-thumbnail"
-                  />
-                </div>
-                <div class="media-body mb-5 text-black">
-                  <h4 class="mt-0 mb-0">Nabil Elbir</h4>
-                  <p class="small mb-4">
-                    <i class="fas fa-map-marker-alt mr-2"></i>Nabeul
-                  </p>
-                </div>
+    <div class="row py-8 px-8">
+      <div class="col-md-20 mx-auto">
+        <div class="bg-white shadow rounded overflow-hidden">
+          <div class="px-4 pt-0 pb-4 cover">
+            <div class="media align-items-end profile-head">
+              <div class="profile mr-3">
+                <img
+                  src="https://apollo.imgix.net/content/uploads/2019/08/5d5c68a43f00005c005b1927.jpeg?auto=compress,format&w=900&h=600"
+                  alt="..."
+                  width="200"
+                  class="rounded mb-2 img-thumbnail"
+                />
+              </div>
+              <div class="media-body mb-5 text-black">
+                <h4 class="mt-0 mb-0">Artist Artist</h4>
               </div>
             </div>
-            <div
-              class="p-4 d-flex justify-content-end text-center"
-              id="heading"
-            ></div>
-            <div class="px-3 py-3">
-              <h5 class="mb-3">About</h5>
-              <div class="p-4 rounded shadow-sm" id="info-card">
-                <p class="font-italic mb-0">Related text</p>
-                <p class="font-italic mb-0">Related text</p>
-                <p class="font-italic mb-0">Related text</p>
-              </div>
+          </div>
+          <div
+            class="p-4 d-flex justify-content-end text-center"
+            id="heading"
+          ></div>
+          <div class="px-3 py-4">
+            <h5 class="mb-4">About</h5>
+            <div class="p-4 rounded shadow-sm" id="info-card">
+              <p class="font-italic mb-0">Artist biography</p>
             </div>
-            <div class="py-3 px-3">
-              <h5 class="mb-3">Artworks</h5>
-              <div
-                class="p-4 rounded shadow-sm"
-                id="artworks-cards"
-                v-for="(artwork, i) in artworks"
-                :key="i"
-              >
-                <div class="artwork-card">
-                  <img class="card-img" v-bind:src="artwork.imageUrl" alt="#" />
-                  <h3>{{ artwork.nameArtwork }}</h3>
-                  <p>Digital Paintings</p>
-                  <p>320 DT</p>
+          </div>
+          <!-- //ARTWORKS SECTION -->
+          <div class="px-3 py-4">
+            <h5 class="mb-4">Artworks</h5>
+            <div class="p-4 rounded shadow-sm" id="artworks">
+              <div class="card-container">
+                <div class="container">
+                  <!-- Top Navigation -->
+                  <div class="content">
+                    <div class="grid">
+                      <figure class="effect-ravi">
+                        <img
+                          id="card-image"
+                          src="https://www.bensalemwalid.com/wp-content/uploads/2021/02/the-fallen-artwork-by-bensalem-walid.png"
+                        />
+                        <figcaption>
+                          <div>
+                            <h2 id="card-title">Refuge</h2>
+                            <p id="card-desc">
+                              Whatever whatever whatever whatever
+                            </p>
+                          </div>
+                        </figcaption>
+                      </figure>
+                      <figure class="effect-ravi">
+                        <img
+                          id="card-image"
+                          src="https://www.bensalemwalid.com/wp-content/uploads/2021/02/silent-tears-artwork-by-bensalem-walid.png"
+                        />
+                        <figcaption>
+                          <div>
+                            <h2 id="card-title">Silent Tears</h2>
+                            <p id="card-desc">
+                              Whatever whatever whatever whatever
+                            </p>
+                          </div>
+                        </figcaption>
+                      </figure>
+                      <figure class="effect-ravi">
+                        <img
+                          id="card-image"
+                          src="https://www.bensalemwalid.com/wp-content/uploads/2021/02/the-dancer-in-red-artwork-by-bensalem-walid.png"
+                        />
+                        <figcaption>
+                          <div>
+                            <h2 id="card-title">The Dancer In Red</h2>
+                            <p id="card-desc">
+                              Whatever whatever whatever whatever
+                            </p>
+                          </div>
+                        </figcaption>
+                      </figure>
+                      <figure class="effect-ravi">
+                        <img
+                          id="card-image"
+                          src="https://www.bensalemwalid.com/wp-content/uploads/2021/02/dreams-artwork-by-bensalem-walid.png"
+                        />
+                        <figcaption>
+                          <div>
+                            <h2 id="card-title">Colors Of My Dreams</h2>
+                            <p id="card-desc">
+                              Whatever whatever whatever whatever
+                            </p>
+                          </div>
+                        </figcaption>
+                      </figure>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
@@ -57,51 +104,27 @@
     </div>
   </div>
 </template>
-<script>
-import NavBar from "./NavBar.vue";
-import axios from "axios";
-export default {
-  data() {
-    return {
-      artworks: [],
-    };
-  },
 
-  components: {
-    NavBar,
-  },
-  methods: {
-    getArtworks() {
-      axios
-        .get(`http://localhost:3000/api/artworks`)
-        .then((res) => {
-          this.artworks = res.data;
-          console.log(res);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    },
-  },
-  mounted() {
-    this.getArtworks();
-  },
-};
+<script>
+export default {};
 </script>
+
 <style scoped>
 @import url("https://fonts.googleapis.com/css2?family=Lexend:wght@100;300;400;500;600;700;800&display=swap");
-
 * {
-  font-family: "Lexend", serif;
+  font-family: "Lexend", sans-serif;
 }
 .profile-head {
+  padding-top: 50px;
   transform: translateY(5rem);
 }
+
 .cover {
   background-color: #fdf5e6;
   background-size: cover;
   background-repeat: no-repeat;
 }
+
 #heading {
   padding: 30px !important;
   background-color: #fdf5e6;
@@ -109,32 +132,179 @@ export default {
 #info-card {
   background-color: #fdf5e6;
 }
-#artworks-cards {
+
+#submitbtn {
+  width: 120px;
+  margin: auto;
+  margin-top: 20px;
+}
+
+#artworks {
   background-color: #fdf5e6;
-  display: flex;
-  flex-wrap: wrap;
-  /* justify-content: center; */
 }
-.artwork-card {
-  width: 300px;
-  max-height: 380px;
-  min-height: 380px;
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  transition: 0.3s;
-  border-radius: 5px;
+
+
+
+
+.grid {
+  position: relative;
+  margin: 0 auto;
+  padding: 1em 0 4em;
+  max-width: 1000px;
+  list-style: none;
   text-align: center;
-  margin: 10px;
-  box-sizing: border-box;
 }
-.artwork-card:hover {
-  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+/* Common style */
+
+.grid figure {
+  position: relative;
+  float: left;
+  overflow: hidden;
+  margin: 10px 1%;
+  min-width: 320px;
+  max-width: 480px;
+  max-height: 360px;
+  width: 48%;
+  background: #3085a3;
+  text-align: center;
+  cursor: pointer;
 }
-.card-img {
+
+.grid figure #card-image {
+  position: relative;
+  display: block;
+  min-height: 100%;
+  max-width: 100%;
+  opacity: 0.8;
+}
+
+.grid figure figcaption {
+  padding: 2em;
+  color: #fff;
+  text-transform: uppercase;
+  font-size: 1.25em;
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+}
+
+.grid figure figcaption::before,
+.grid figure figcaption::after {
+  pointer-events: none;
+}
+
+.grid figure figcaption,
+.grid figure figcaption > a {
+  position: absolute;
+  top: 0;
+  left: 0;
   width: 100%;
-  max-height: 240px;
-  min-height: 240px;
-  object-fit: cover;
-  border-radius: 5px 5px 0px 0px;
-  margin-bottom: 15px;
+  height: 100%;
 }
-</style> 
+/* Anchor will cover the whole item by default */
+/* For some effects it will show as a button */
+
+.grid figure figcaption > a {
+  z-index: 1000;
+  text-indent: 200%;
+  white-space: nowrap;
+  font-size: 0;
+  opacity: 0;
+}
+
+.grid figure #card-title {
+  word-spacing: -0.15em;
+  font-weight: 300;
+}
+
+.grid figure #card-title {
+  font-weight: 800;
+}
+
+.grid figure #card-title,
+.grid figure #card-desc {
+  margin: 0;
+}
+
+.grid figure #card-desc {
+  letter-spacing: 1px;
+  font-size: 68.5%;
+}
+/* Individual effects */
+/*---------------*/
+/***** Lily *****/
+/*---------------*/
+
+figure.effect-ravi #card-image {
+  max-width: none;
+  width: -webkit-calc(100% + 50px);
+  width: calc(100% + 50px);
+  opacity: 0.7;
+  -webkit-transition: opacity 0.35s, -webkit-transform 0.35s;
+  transition: opacity 0.35s, transform 0.35s;
+  -webkit-transform: translate3d(-40px, 0, 0);
+  transform: translate3d(-40px, 0, 0);
+}
+
+figure.effect-ravi figcaption {
+  text-align: left;
+}
+
+figure.effect-ravi figcaption > div {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  padding: 2em;
+  width: 100%;
+  height: 50%;
+}
+
+figure.effect-ravi #card-title,
+figure.effect-ravi #card-desc {
+  -webkit-transform: translate3d(0, 40px, 0);
+  transform: translate3d(0, 40px, 0);
+}
+
+figure.effect-ravi #card-title {
+  -webkit-transition: -webkit-transform 0.35s;
+  transition: transform 0.35s;
+}
+
+figure.effect-ravi #card-desc {
+  color: rgba(255, 255, 255, 0.8);
+  opacity: 0;
+  -webkit-transition: opacity 0.2s, -webkit-transform 0.35s;
+  transition: opacity 0.2s, transform 0.35s;
+}
+
+figure.effect-ravi:hover #card-image,
+figure.effect-ravi:hover #card-desc {
+  opacity: 1;
+}
+
+figure.effect-ravi:hover #card-image,
+figure.effect-ravi:hover #card-title,
+figure.effect-ravi:hover #card-desc {
+  -webkit-transform: translate3d(0, 0, 0);
+  transform: translate3d(0, 0, 0);
+}
+
+figure.effect-ravi:hover #card-desc {
+  -webkit-transition-delay: 0.05s;
+  transition-delay: 0.05s;
+  -webkit-transition-duration: 0.35s;
+  transition-duration: 0.35s;
+}
+
+@media screen and (max-width: 50em) {
+  .content {
+    padding: 0 10px;
+    text-align: center;
+  }
+  .grid figure {
+    display: inline-block;
+    float: none;
+    margin: 10px auto;
+    width: 100%;
+  }
+}
+</style>
