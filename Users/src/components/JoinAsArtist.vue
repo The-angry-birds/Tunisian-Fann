34cf6cab8b5d80a12909ec36eb6bcd759ce9c1b1
MalekWahmi <wahmimalek@gmail.com>
2021-04-02 19:19:11 +0100

    fix-adminSide

--- Users/src/components/JoinAsArtist.vue
+++ Users/src/components/JoinAsArtist.vue
@@ -109,16 +109,23 @@ export default {
         //TODO: mapDispatch()
         this.$store
           .dispatch("register", data)
-          .then(() => {
-            this.$router.push("/artist-profile");
-            Swal.fire({
-              position: "top-end",
-              icon: "success",
-              title: "Your work has been saved!",
-              showConfirmButton: false,
-              timer: 1500,
-            });
+          .then((res) => {
+            if (res.data.user.accept === false) {
+              console.log("===============>",res.data.user.accept)
+              swal("Oops!", "You are Not Accepted", "error");
+              this.$router.push("/");
+            } else {
+              this.$router.push("/artist-profile");
+              Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: "Your work has been saved!",
+                showConfirmButton: false,
+                timer: 1500,
+              });
+            }
           })
+
           .catch((err) => {
             console.log(err);
             swal("oops", "Something went wrong!");
@@ -138,9 +145,8 @@ export default {
               swal("Oops!", "Wrong Password!", "error");
             } else if (resp.message === "user not found") {
               swal("Oops!", "Wrong e-mail!", "error");
-
             } else if (resp.user.accept === false) {
-              swal("Oops!", "You are Not Accepted", "error");
+              swal("Oops!", "You need to be verified by the Admin team!", "error");
             } else if (resp.user.banned === true) {
               swal("Oops!", "You are banned!", "error");
             } else {
