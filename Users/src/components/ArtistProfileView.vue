6ed6bf2c83d4ade97a771e12829f4c9a2df50a91
Nabil-Elbir <nabil.elbir@gmail.com>
2021-04-05 15:31:33 +0100

    fixed smthg

--- Users/src/components/ArtistProfileView.vue
+++ Users/src/components/ArtistProfileView.vue
@@ -32,74 +32,78 @@
           <!-- //ARTWORKS SECTION -->
           <div class="px-3 py-4">
             <h5 class="mb-4">Artworks</h5>
-            <div class="p-4 rounded shadow-sm" id="artworks"></div>
-          </div>
-          <div class="card-container">
-            <card class="artwork-card" v-for="(e, i) in artworks" :key="i">
-              <img class="img-card" :src="e.imageUrl" />
-              <h3 class="card-title">{{ e.nameArtwork }}</h3>
-              <p class="card-category">{{ e.description.substr(0, 10) }}...</p>
-              <h6 class="card-price">{{ e.price }} dt</h6>
-              <div class="card-by">
-                by
-                <p class="card-author">
-                  {{ artist.firstName }} {{ artist.lastName }}
-                </p>
+            <div class="p-4 rounded shadow-sm" id="artworks">
+              <div class="card-container">
+                <card class="artwork-card" v-for="(e, i) in artworks" :key="i">
+                  <img class="img-card" :src="e.imageUrl" />
+                  <h3 class="card-title">{{ e.nameArtwork }}</h3>
+                  <p class="card-category">
+                    {{ e.description.substr(0, 10) }}...
+                  </p>
+                  <h6 class="card-price">{{ e.price }} dt</h6>
+                  <div class="card-by">
+                    by
+                    <p class="card-author">
+                      {{ artist.firstName }} {{ artist.lastName }}
+                    </p>
+                  </div>
+                  
+                </card>
               </div>
-              <div></div>
-            </card>
+            </div>
           </div>
         </div>
-      </div>
-      <div class="px-3 py-4">
-        <h5 class="mb-4">Auctions</h5>
-        <div class="p-4 rounded shadow-sm" id="auctions">
-          <div class="container" v-for="(auction, i) in auctionData" :key="i">
-            <article class="card card--1">
-              <div class="card__info-hover">
-                <svg class="card__like" viewBox="0 0 24 24">
-                  <path
-                    fill="#000000"
-                    d="M12.1,18.55L12,18.65L11.89,18.55C7.14,14.24 4,11.39 4,8.5C4,6.5 5.5,5 7.5,5C9.04,5 10.54,6 11.07,7.36H12.93C13.46,6 14.96,5 16.5,5C18.5,5 20,6.5 20,8.5C20,11.39 16.86,14.24 12.1,18.55M16.5,3C14.76,3 13.09,3.81 12,5.08C10.91,3.81 9.24,3 7.5,3C4.42,3 2,5.41 2,8.5C2,12.27 5.4,15.36 10.55,20.03L12,21.35L13.45,20.03C18.6,15.36 22,12.27 22,8.5C22,5.41 19.58,3 16.5,3Z"
-                  />
-                </svg>
-                <div class="card__clock-info">
-                  <svg class="card__clock" viewBox="0 0 24 24">
+
+        <div class="px-3 py-4">
+          <h5 class="mb-4">Auctions</h5>
+          <div class="p-4 rounded shadow-sm" id="auctions">
+            <div class="container" v-for="(auction, i) in auctionData" :key="i">
+              <article class="card card--1">
+                <div class="card__info-hover">
+                  <svg class="card__like" viewBox="0 0 24 24">
                     <path
-                      d="M12,20A7,7 0 0,1 5,13A7,7 0 0,1 12,6A7,7 0 0,1 19,13A7,7 0 0,1 12,20M19.03,7.39L20.45,5.97C20,5.46 19.55,5 19.04,4.56L17.62,6C16.07,4.74 14.12,4 12,4A9,9 0 0,0 3,13A9,9 0 0,0 12,22C17,22 21,17.97 21,13C21,10.88 20.26,8.93 19.03,7.39M11,14H13V8H11M15,1H9V3H15V1Z"
+                      fill="#000000"
+                      d="M12.1,18.55L12,18.65L11.89,18.55C7.14,14.24 4,11.39 4,8.5C4,6.5 5.5,5 7.5,5C9.04,5 10.54,6 11.07,7.36H12.93C13.46,6 14.96,5 16.5,5C18.5,5 20,6.5 20,8.5C20,11.39 16.86,14.24 12.1,18.55M16.5,3C14.76,3 13.09,3.81 12,5.08C10.91,3.81 9.24,3 7.5,3C4.42,3 2,5.41 2,8.5C2,12.27 5.4,15.36 10.55,20.03L12,21.35L13.45,20.03C18.6,15.36 22,12.27 22,8.5C22,5.41 19.58,3 16.5,3Z"
                     />
                   </svg>
-                  <span v-if="!isExpired" class="card__time" id="demo">
-                    {{ distanceDate.days }}Days {{ distanceDate.hours }}Hr
-                    {{ distanceDate.minutes }}Min {{ distanceDate.seconds }}Sec
-                    : Left</span
-                  >
-                  <!-- <span v-if="isExpired" class="card__time" id="demo"> Expired </span> -->
+                  <div class="card__clock-info">
+                    <svg class="card__clock" viewBox="0 0 24 24">
+                      <path
+                        d="M12,20A7,7 0 0,1 5,13A7,7 0 0,1 12,6A7,7 0 0,1 19,13A7,7 0 0,1 12,20M19.03,7.39L20.45,5.97C20,5.46 19.55,5 19.04,4.56L17.62,6C16.07,4.74 14.12,4 12,4A9,9 0 0,0 3,13A9,9 0 0,0 12,22C17,22 21,17.97 21,13C21,10.88 20.26,8.93 19.03,7.39M11,14H13V8H11M15,1H9V3H15V1Z"
+                      />
+                    </svg>
+                    <span v-if="!isExpired" class="card__time" id="demo">
+                      {{ distanceDate.days }}Days {{ distanceDate.hours }}Hr
+                      {{ distanceDate.minutes }}Min
+                      {{ distanceDate.seconds }}Sec : Left</span
+                    >
+                    <!-- <span v-if="isExpired" class="card__time" id="demo"> Expired </span> -->
+                  </div>
                 </div>
-              </div>
-              <img class="card__img" src="" />
-              <a href="#" class="card_link">
-                <img
-                  class="card__img--hover"
-                  :src="auction.imageUrl"
-                  @click="sharedData(auction)"
-                />
-              </a>
-              <div class="card__info">
-                <h3 class="card__title">{{ auction.nameArtwork }}</h3>
-                <span class="card__category">
-                  {{ auction.description }}
-                </span>
-                <span class="card__by"
-                  ><br />
-                  by
-                  <a href="#" class="card__author" title="author"
-                    >{{ artist.firstName }} {{ artist.lastName }}</a
-                  ></span
-                >
-                >
-              </div>
-            </article>
+                <img class="card__img" src="" />
+                <a href="#" class="card_link">
+                  <img
+                    class="card__img--hover"
+                    :src="auction.imageUrl"
+                    @click="sharedData(auction)"
+                  />
+                </a>
+                <div class="card__info">
+                  <h3 class="card__title">{{ auction.nameArtwork }}</h3>
+                  <span class="card__category">
+                    {{ auction.description.substr(0, 10) }}...
+                  </span>
+                  <span class="card__by"
+                    ><br />
+                    by
+                    <a href="#" class="card__author" title="author"
+                      >{{ artist.firstName }} {{ artist.lastName }}</a
+                    ></span
+                  >
+                  >
+                </div>
+              </article>
+            </div>
           </div>
         </div>
       </div>
